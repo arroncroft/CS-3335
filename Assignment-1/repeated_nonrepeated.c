4bfee3dc8c6fc3ae5d41fa7059b0b14e9ee780ab
arroncroft <arroncroft@hotmail.com>
2017-08-19 20:22:39 -0400

    Finished Assignment 1

--- Assignment-1/repeated_nonrepeated.c
+++ Assignment-1/repeated_nonrepeated.c
@@ -1,22 +1,95 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
+	
 	// Change the elements in the following array
 	// to test your program
 	int digits[]={2,0,38,2,3,1,3};
 	int i;
+	bool found = false;
 
-	printf("In the array of ");	
+	printf("In the array of: ");	
 	for (i=0; i<sizeof(digits)/sizeof(int); i++)
 		printf("%d ", digits[i]);
-	printf("\n");	
+	printf("\n\n");	
+	
+	//first non-repeated digit
+	for (i=0; i<sizeof(digits)/sizeof(int); i++) {
+		for (int j=i+1; j<sizeof(digits)/sizeof(int); j++) {
+			found = false;
+			if (digits[i] == digits[j]) {
+				found = true;
+				break;
+			}
+		}
+		if (found == false) {
+			printf("The first non-repeated digit is %d.", digits[i]);
+			break;
+		}
+	}
+	if (found == true) {
+			printf("There is no non-repeated digit.");
+		}
+	printf("\n");
+	
+	//last non-repeated digit
+	for (i=(sizeof(digits)/sizeof(int))-1; i>=0; i--) {
+		for (int j=i-1; j>=0; j--) {
+			found = false;
+			if (digits[i] == digits[j]) {
+				found = true;
+				break;
+			}
+		}
+		if (found == false) {
+			printf("The last non-repeated digit is %d.", digits[i]);
+			break;
+		}
+	}
+	if (found == true) {
+		printf("There is no non-repeated digit.");
+	}
+	printf("\n");
 
-	// Your code here...
-	int digits2[100]; //maybe?
-	printf("The first non-repeated digit is ");
-	printf("The last non-repeated digit is ");
-	printf("The first repeated digit is ");
-	printf("The last repeated digit is ");
+	//first repeated digit
+	found = false;
+	for (i=0; i<sizeof(digits)/sizeof(int); i++) {
+		for (int j=i+1; j<sizeof(digits)/sizeof(int); j++) {
+			found = false;
+			if (digits[i] == digits[j]){
+				found = true;
+				break;
+			}
+		}
+		if (found == true) {
+			printf("The first repeated digit is %d.", digits[i]);
+		}
+		else {
+			printf("There is no repeated digit.");
+		}
+		break;
+	}
+	printf("\n");
+
+	//last repeated digit
+	for (i=(sizeof(digits)/sizeof(int))-1; i>=0; i--) {
+		for (int j=i-1; j>=0; j--) {
+			found = false;
+			if (digits[i] == digits[j]){
+				found = true;
+				break;
+			}
+		}
+		if (found == true) {
+			printf("The last repeated digit is %d.", digits[i]);
+		}
+		else {
+			printf("There is no repeated digit.");
+		}
+		break;
+	}
+	printf("\n");
 
 	return 0;
 }
