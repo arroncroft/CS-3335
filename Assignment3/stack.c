5de53f118e3041ee96bda37c3dba247256571aab
arroncroft <arroncroft@hotmail.com>
2017-09-14 08:55:38 -0400

    finished Assignment 3

--- Assignment3/stack.c
+++ Assignment3/stack.c
@@ -22,6 +22,13 @@ void push(char stack[],
 	// Your code here ...
 	// Push item to the top of stack if it's not full. 
 	// Update the top pointer s/t it points to the index of the new top element on the stack.
+
+	//my code
+	if (*top < STACK_SIZE - 1) { //if stack is not full
+		stack[*top + 1] = item; // push to top of stack
+		*top = *top + 1; //set pointer to top of stack
+	}
+	//my code ends
 }
 
 char pop(char stack[],
@@ -33,26 +40,52 @@ char pop(char stack[],
 	// 
 	// Return STACK_EMPTY if the stack is empty. 	
 
+	//my code
+	if (*top > STACK_EMPTY) {
+		char popped = stack[*top];
+		stack[*top] = ' ';
+		*top = *top - 1;
+		return popped;
+	}
+	//my code ends
+
 	return STACK_EMPTY;
 }
 
 int main(){
 	char s[STACK_SIZE];
 	int s_top = STACK_EMPTY; // The ***index*** of the top of the stack
+	int i;
 
 	srand(time(NULL));
 
 	// Your code here ...
 	// (Using a loop) Keep pushing chars randomly picked between '!'(33) and '~'(126) to the stack
 	// until it is full. You will find the function rand() useful here. In case you are not familiar
-   	// with it, man it.
+	// with it, man it.
 	// Print each char before each pushing.
+
+	//my code
+	srand ( time(0) );	
+	char random = rand() % 93 + 33;
+	for (i = 0; i < STACK_SIZE; i++) {
+		random = rand() % 93 + 33;
+		printf("%c\n", random);
+		push(s, random, &s_top, STACK_SIZE);
+	}
+	//my code ends
 
 	printf("-----------\n");
 
 	// Your code here ...
 	// (Using a loop) Keep popping out chars from the stack until it is empty
 	// Print each char after each popping. 
+
+	//my code
+	for (i = 0; i < STACK_SIZE; i++) {
+		printf("%c\n", pop(s, &s_top));
+	}
+	//my code ends
 
 	return 0;
 }
