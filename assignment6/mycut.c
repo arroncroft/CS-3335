b518b4c495997a5bb1d8475f9ca05ef3db41e6e9
arroncroft <arroncroft@hotmail.com>
2017-10-08 15:20:28 -0400

    worked on a6

--- assignment6/mycut.c
+++ assignment6/mycut.c
@@ -1,12 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 	// Gather the command line arguments (options included) and combine them in one string.
-	// Create a command string that begins with "cut ", followed by the argument string above. 
-	// Finally replace the hardwired "cut ...." in the following system call with your command string.  
-	system("cut -d! -f3 emp.lst");
-	
+	// Create a command string that begins with "cut ", followed by the argument string above.
+	// Finally replace the hardwired "cut ...." in the following system call with your command string.
+	// system("cut -d! -f3 emp.lst");
+
+	char ch;
+	char *d = "";
+	char *f = "";
+	int count = 0;
+
+	while ((ch = getopt(argc, argv, "d:f:")) != EOF)
+	{
+		switch (ch)
+		{
+		case 'd':
+			d = optarg;
+			break;
+		case 'f':
+			f = optarg;
+			break;
+		default:
+			fprintf(stderr, "unknown option: '%s'\n", optarg);
+			return 1;
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
+
+	if(d[0])
+		printf("testing\n");
+	if(f[0])
+		printf("testing 2\n");
+
 	return 0;
 }
