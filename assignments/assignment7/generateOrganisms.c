5d23a06ca313e1cf78c5c632bab69cb6d57f4d31
arroncroft <arroncroft@hotmail.com>
2017-10-25 00:51:53 -0400

    mostly finished a7 in C

--- assignments/assignment7/generateOrganisms.c
+++ assignments/assignment7/generateOrganisms.c
@@ -24,32 +24,111 @@ void generateOrganisms(int percentOrgs, int a, int b, char board[][b])
         {
             //count neighbors
             neighbors = 0;
-            if (board[i][j - 1] == 'X')
-                neighbors++;
-            if (board[i + 1][j - 1] == 'X')
-                neighbors++;
-            if (board[i - 1][j - 1] == 'X')
-                neighbors++;
-            if (board[i][j + 1] == 'X')
-                neighbors++;
-            if (board[i + 1][j + 1] == 'X')
-                neighbors++;
-            if (board[i - 1][j + 1] == 'X')
-                neighbors++;
-            if (board[i + 1][j] == 'X')
-                neighbors++;
-            if (board[i - 1][j] == 'X')
-                neighbors++;
+            if ((i != 0) && (i != a - 1) && (j != 0) && (j != b - 1)) //inside rows/columns
+            {
+                if (board[i + 1][j] == 'X') //up
+                    neighbors++;
+                if (board[i - 1][j] == 'X') //down
+                    neighbors++;
+                if (board[i][j - 1] == 'X') //left
+                    neighbors++;
+                if (board[i + 1][j - 1] == 'X') //up left
+                    neighbors++;
+                if (board[i - 1][j - 1] == 'X') //down left
+                    neighbors++;
+                if (board[i][j + 1] == 'X') //right
+                    neighbors++;
+                if (board[i + 1][j + 1] == 'X') //up right
+                    neighbors++;
+                if (board[i - 1][j + 1] == 'X') //down right
+                    neighbors++;
+            }
+            if (i == 0 && j != 0 && j != b - 1) //top row
+            {
+                if (board[a - 1][j] == 'X') //up
+                    neighbors++;
+                if (board[i - 1][j] == 'X') //down
+                    neighbors++;
+                if (board[i][j - 1] == 'X') //left
+                    neighbors++;
+                if (board[a - 1][j - 1] == 'X') //up left
+                    neighbors++;
+                if (board[i - 1][j - 1] == 'X') //down left
+                    neighbors++;
+                if (board[i][j + 1] == 'X') //right
+                    neighbors++;
+                if (board[a - 1][j + 1] == 'X') //up right
+                    neighbors++;
+                if (board[i - 1][j + 1] == 'X') //down right
+                    neighbors++;
+            }
+            if (i == a - 1 && j != 0 && j != b - 1) //bottom row
+            {
+                if (board[i + 1][j] == 'X') //up
+                    neighbors++;
+                if (board[0][j] == 'X') //down
+                    neighbors++;
+                if (board[i][j - 1] == 'X') //left
+                    neighbors++;
+                if (board[i + 1][j - 1] == 'X') //up left
+                    neighbors++;
+                if (board[0][j - 1] == 'X') //down left
+                    neighbors++;
+                if (board[i][j + 1] == 'X') //right
+                    neighbors++;
+                if (board[i + 1][j + 1] == 'X') //up right
+                    neighbors++;
+                if (board[0][j + 1] == 'X') //down right
+                    neighbors++;
+            }
+            if (j == 0 && i != 0 && i != a - 1) //left column
+            {
+                if (board[i + 1][j] == 'X') //up
+                    neighbors++;
+                if (board[i - 1][j] == 'X') //down
+                    neighbors++;
+                if (board[i][b - 1] == 'X') //left
+                    neighbors++;
+                if (board[i + 1][b - 1] == 'X') //up left
+                    neighbors++;
+                if (board[i - 1][b - 1] == 'X') //down left
+                    neighbors++;
+                if (board[i][j + 1] == 'X') //right
+                    neighbors++;
+                if (board[i + 1][j + 1] == 'X') //up right
+                    neighbors++;
+                if (board[i - 1][j + 1] == 'X') //down right
+                    neighbors++;
+            }
+            if (j == b - 1 && i != 0 && i != a - 1) //right column
+            {
+                if (board[i + 1][j] == 'X') //up
+                    neighbors++;
+                if (board[i - 1][j] == 'X') //down
+                    neighbors++;
+                if (board[i][j - 1] == 'X') //left
+                    neighbors++;
+                if (board[i + 1][j - 1] == 'X') //up left
+                    neighbors++;
+                if (board[i - 1][j - 1] == 'X') //down left
+                    neighbors++;
+                if (board[i][0] == 'X') //right
+                    neighbors++;
+                if (board[i + 1][0] == 'X') //up right
+                    neighbors++;
+                if (board[i - 1][0] == 'X') //down right
+                    neighbors++;
+            }
 
             //death
             if (board[i][j] == 'X')
             {
-                if (neighbors != 2 && neighbors != 3)
-                    board[i][j] == ' ';
+                if (neighbors < 2 || neighbors > 3)
+                    board[i][j] = ' ';
             }
             //birth
-            else if (board[i][j] == ' ')
-            {   
+            if (board[i][j] == ' ')
+            {
                 int r = (rand() % 100) + 1;
                 if (neighbors == 3 && r <= percentOrgs)
                     board[i][j] = 'X';
